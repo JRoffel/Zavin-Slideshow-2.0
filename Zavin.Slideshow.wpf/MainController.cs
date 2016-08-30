e3c02b1f2ee852d99e9078220ccb245db08c3b93
Jason Roffel <jasonroffel@hotmail.nl>
2016-08-30 14:30:08 +0200

    MainController + dummy data

---
+++ Zavin.Slideshow.wpf/MainController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+
+namespace Zavin.Slideshow.wpf
+{
+    class MainController
+    {
+        public DataTable xDataControl = new DataTable();
+
+        public void Echo()
+        {
+            Console.WriteLine("Echo Echo... I am alive");
+        }
+    }
+}
--- Zavin.Slideshow.wpf/MainWindow.xaml.cs
+++ Zavin.Slideshow.wpf/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,35 @@ namespace Zavin.Slideshow.wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        MainController mainController = new MainController();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void MainWindow1_Loaded(object sender, RoutedEventArgs e)
+        {
+            mainController.xDataControl.Rows.Clear();
+            mainController.xDataControl.Columns.Clear();
+            MainChart.Series.Clear();
+            
+            mainController.xDataControl.Columns.Add("X", typeof(int));
+            mainController.xDataControl.Columns.Add("Productie", typeof(int));
+            mainController.xDataControl.Columns.Add("Aanvoer", typeof(int));
+            
+            for (int i = 1; i < 54; i++)
+                            {
+                DataRow dataRow = mainController.xDataControl.NewRow();
+                
+                dataRow["X"] = i;
+                dataRow["Productie"] = i * 12 - 10 + 33;
+                dataRow["Aanvoer"] = i * 9 - 6 + 24;
+                mainController.xDataControl.Rows.Add(dataRow);
+            }
+
+            var convertedTable = (mainController.xDataControl as IListSource).GetList();
+            //MainChart.DataBindTable(convertedTable, "X");
+            MainChart.SetBinding(convertedTable, "X");
         }
     }
 }
