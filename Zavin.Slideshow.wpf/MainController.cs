5e126eefd1538cfcf4154efb0f9b5cb8f5c4ab10
jason roffel <jasonroffel@hotmail.nl>
2016-09-01 15:04:04 +0200

    Demo graph, dummy data, cleanup required

--- Zavin.Slideshow.wpf/MainController.cs
+++ Zavin.Slideshow.wpf/MainController.cs
@@ -5,8 +5,6 @@ namespace Zavin.Slideshow.wpf
 {
     class MainController
     {
-        public DataTable xDataControl = new DataTable();
-
         public void Echo()
         {
             Console.WriteLine("Echo Echo... I am alive");
