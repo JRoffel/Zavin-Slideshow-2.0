6c95a67a359d1490895e5f10d88c107ea65777cd
Angelo Roks <angeloroks@hotmail.com>
2016-09-06 14:57:00 +0200

    Moved weekgraph from mainwindow to seperate page called WeekGraphPage, Piegraph has been added to YearReviewPage.

---
+++ Zavin.Slideshow.wpf/YearGraphPage.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.DataVisualization.Charting;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Zavin.Slideshow.wpf
+{
+    /// <summary>
+    /// Interaction logic for YearGraphPage.xaml
+    /// </summary>
+    public partial class YearGraphPage : Page
+    {
+        public YearGraphPage()
+        {
+            InitializeComponent();
+        }
+        private void LoadPieChartData(object sender, RoutedEventArgs e)
+        {
+
+            ((PieSeries)PieChart.Series[0]).ItemsSource =
+
+                new KeyValuePair<string, int>[]
+                {
+                    new KeyValuePair<string, int>("verbrand", 6629),
+                    new KeyValuePair<string, int>("Begroting", 3347),
+                    new KeyValuePair<string, int>("Extra", 1109)
+                };
+
+            //((PieSeries)PieChart.Series[1]).ItemsSource =
+
+            //    new KeyValuePair<string, int>[]
+            //    {
+            //        new KeyValuePair<string,int>("Overig",3271)
+            //    };
+
+            //((PieSeries)PieChart.Series[2]).ItemsSource =
+
+            //    new KeyValuePair<string, int>[]
+            //    {
+            //        new KeyValuePair<string,int>("Test",3838)
+            //    };
+        }
+    }
+}
