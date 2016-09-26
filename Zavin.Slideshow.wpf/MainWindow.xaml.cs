c7f0f828cdb444119694458f11d172303f37eb2d
Lucas Assink <Lucas Assink>
2016-09-26 11:08:31 +0200

    create branch

--- Zavin.Slideshow.wpf/MainWindow.xaml.cs
+++ Zavin.Slideshow.wpf/MainWindow.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using System;
+using System.Net;
+using System.Xml;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Linq;
 using System.Text;
+using System.Diagnostics;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,9 +14,11 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 using System.Windows.Controls.DataVisualization.Charting;
 
 namespace Zavin.Slideshow.wpf
@@ -28,6 +33,27 @@ namespace Zavin.Slideshow.wpf
         public MainWindow()
         {
             InitializeComponent();
+            int bySizeTemp = Convert.ToInt32(headlines.ActualWidth);
+            int bySize = bySizeTemp - bySizeTemp * 2;
+            test.Text = bySizeTemp.ToString();
+
+            for (int i = 0; i < headlines.Items.Count; i++)
+            {
+
+                ContentPresenter c = (ContentPresenter)headlines.ItemContainerGenerator.ContainerFromItem(headlines.Items[i]);
+                TextBlock tb = c.ContentTemplate.FindName("pancake", c) as TextBlock;
+
+                MessageBox.Show("  pancake  "
+    + tb.Text);
+            }
+
+            //DoubleAnimation slide = new DoubleAnimation();
+            //slide.From = 1920;
+            //slide.By = bySize;
+            //slide.Duration = TimeSpan.FromMilliseconds(4000);
+            //headlinetitles.BeginAnimation(Canvas.WidthProperty, slide);
+
+
         }
 
         private void MainWindow1_KeyDown(object sender, KeyEventArgs e)
@@ -37,5 +63,8 @@ namespace Zavin.Slideshow.wpf
                 Application.Current.Shutdown();
             }
         }
-    }
+
+        
+
+}
 }
