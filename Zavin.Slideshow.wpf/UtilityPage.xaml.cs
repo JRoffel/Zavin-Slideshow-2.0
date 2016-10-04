ac6ff33fb203304cc5ee869c18d00ac2be84feb2
Angelo Roks <angeloroks@hotmail.com>
2016-10-04 11:26:19 +0200

    Added traffic rss feed

--- Zavin.Slideshow.wpf/UtilityPage.xaml.cs
+++ Zavin.Slideshow.wpf/UtilityPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Xml.Linq;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Globalization;
+using System.Windows.Markup;
 
 namespace Zavin.Slideshow.wpf
 {
@@ -20,9 +23,33 @@ namespace Zavin.Slideshow.wpf
     /// </summary>
     public partial class UtilityPage : Page
     {
+    
         public UtilityPage()
         {
             InitializeComponent();
+     
         }
     }
+    [ValueConversion(typeof(object), typeof(string))]
+    public class DataSanitizer : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strValue = (string)value;
+            return strValue.Replace("Bron: Verkeerplaza.nl: ", "");
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string strValue = (string)value;
+            return strValue.Replace("Bron: Verkeerplaza.nl: ", "");
+        }
+    }
+
+    //public abstract class BaseConverter : MarkupExtension
+    //{
+    //    public override object ProvideValue(IServiceProvider serviceProvider)
+    //    {
+    //        return this;
+    //    }
+    //}
 }
