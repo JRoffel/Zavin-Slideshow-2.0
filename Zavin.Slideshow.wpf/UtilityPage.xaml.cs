2f8eb7a1feb9fe0fcfff68bb54a5481e0d9887d5
Angelo Roks <angeloroks@hotmail.com>
2016-10-06 16:24:33 +0200

    Remade the entire rss feed canvas

--- Zavin.Slideshow.wpf/UtilityPage.xaml.cs
+++ Zavin.Slideshow.wpf/UtilityPage.xaml.cs
@@ -18,6 +18,7 @@ using System.Windows.Markup;
 using System.ComponentModel;
 using System.Runtime.Serialization;
 using System.IO;
+using System.Windows.Media.Animation;
 
 namespace Zavin.Slideshow.wpf
 {
@@ -26,6 +27,8 @@ namespace Zavin.Slideshow.wpf
     /// </summary>
     public partial class UtilityPage : Page
     {
+        public static List<string> titles;
+        public static List<string> descs;
 
         public UtilityPage()
         {
@@ -34,9 +37,46 @@ namespace Zavin.Slideshow.wpf
             string curDir = Directory.GetCurrentDirectory();
             var test = (String.Format("file:///{0}/weather.html", curDir));
             wbWeather.Address = test;
+            GetAndSetTrafficRss();
+
+        //DoubleAnimation TrafficAnimation = new DoubleAnimation();
+        //TrafficAnimation.From = 0;
+        //TrafficAnimation.To = TrafficControl.ActualHeight / 2;
+        //TrafficAnimation.Duration = TimeSpan.FromMilliseconds(4000);
+        //TrafficControl.BeginAnimation(Canvas.HeightProperty, TrafficAnimation);
+
         }
+
+        private void GetAndSetTrafficRss()
+        {
+           
+            XDocument doc = XDocument.Load("http://www.verkeerplaza.nl/rssfeed");
+            titles = (from x in doc.Descendants("item")
+                     select x.Element("title").Value).ToList();
+            descs = (from x in doc.Descendants("item")
+                     select x.Element("description").Value).ToList();
+
+            for (int i = 0; i < titles.Count; i++)
+            {
+                TextBlock trafficTitle = new TextBlock();
+                TextBlock trafficDesc = new TextBlock();
+                if (titles[i].Contains("Bron: Verkeerplaza.nl: "))
+                {
+                    titles[i].Replace("Bron: Verkeerplaza.nl: ", "");
+                }
+                trafficTitle.Text = titles[i];
+                trafficTitle.FontSize = 20;
+                trafficTitle.Margin = new Thickness(30, 10, 50, 0);
+                trafficDesc.Text = descs[i];
+                trafficDesc.FontSize = 15;
+                trafficDesc.Margin = new Thickness(90, 10, 10, 30);
+                trafficPanel.Children.Add(trafficTitle);
+                trafficPanel.Children.Add(trafficDesc);
+            }
+        }
+
     }
-    
+
 
     [ValueConversion(typeof(object), typeof(string))]
     public class DataSanitizer : IValueConverter
@@ -52,6 +92,7 @@ namespace Zavin.Slideshow.wpf
             return strValue.Replace("Bron: Verkeerplaza.nl: ", "");
         }
     }
+
 
     //public abstract class BaseConverter : MarkupExtension
     //{
